82882374018feaa2944d3fa4a3de58ca97879981
bgrcdnc <bugracaliskandinc@gmail.com>
2024-09-01 15:12:40 +0300

    Add TodoerService impl

--- src/main/java/com/bugracdnc/todoerserver/services/TodoerService.java
+++ src/main/java/com/bugracdnc/todoerserver/services/TodoerService.java
@@ -1,4 +1,19 @@
 package com.bugracdnc.todoerserver.services;
 
+import com.bugracdnc.todoerserver.models.TodoerDTO;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface TodoerService {
+    List<TodoerDTO> list();
+
+    TodoerDTO save(TodoerDTO dto);
+
+    Optional<TodoerDTO> getById(UUID id);
+
+    Optional<TodoerDTO> updateById(UUID todoId, TodoerDTO dto);
+
+    boolean deleteById(UUID todoId);
 }
--- src/main/java/com/bugracdnc/todoerserver/services/TodoerServiceJPA.java
+++ src/main/java/com/bugracdnc/todoerserver/services/TodoerServiceJPA.java
@@ -1,13 +1,67 @@
 package com.bugracdnc.todoerserver.services;
 
+import com.bugracdnc.todoerserver.entities.Todoer;
 import com.bugracdnc.todoerserver.mappers.TodoerMapper;
+import com.bugracdnc.todoerserver.models.TodoerDTO;
 import com.bugracdnc.todoerserver.repo.TodoerRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 @RequiredArgsConstructor
 public class TodoerServiceJPA implements TodoerService {
     private final TodoerRepo todoerRepo;
     private final TodoerMapper todoerMapper;
+
+    @Override
+    public boolean deleteById(UUID todoId) {
+        if(todoerRepo.existsById(todoId)) {
+            todoerRepo.deleteById(todoId);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Optional<TodoerDTO> updateById(UUID todoId, TodoerDTO dto) {
+        AtomicReference<Optional<TodoerDTO>> atomicReference = new AtomicReference<>();
+        todoerRepo.findById(todoId).ifPresentOrElse(todoer -> {
+            todoer.setTodo(dto.getTodo());
+            todoer.setCreatedDate(dto.getCreatedDate());
+            todoer.setUpdateDate(LocalDateTime.now());
+            atomicReference.set((Optional.of(todoerMapper.objToDto(todoerRepo.save(todoer)))));
+        }, () -> {
+            atomicReference.set(Optional.empty());
+        });
+
+        return atomicReference.get();
+    }
+
+    @Override
+    public TodoerDTO save(TodoerDTO dto) {
+        Todoer todoer = todoerMapper.dtoToObj(dto);
+        todoer.setId(UUID.randomUUID());
+        todoer.setCreatedDate(LocalDateTime.now());
+        todoer.setUpdateDate(LocalDateTime.now());
+        return todoerMapper.objToDto(todoerRepo.save(todoer));
+    }
+
+    @Override
+    public Optional<TodoerDTO> getById(UUID id) {
+        return Optional.ofNullable(todoerMapper.objToDto(todoerRepo.findById(id).orElse(null)));
+    }
+
+    @Override
+    public List<TodoerDTO> list() {
+        return todoerRepo.findAll()
+                         .stream()
+                         .map(todoerMapper::objToDto)
+                         .toList();
+    }
 }
