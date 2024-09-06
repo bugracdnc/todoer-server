fbf08e095837859b5dc7936e8a6f54f2ef2dca5d
bugracdnc <bugracaliskandinc@gmail.com>
2024-09-07 00:44:13 +0300

    Update import optimization

--- src/main/java/com/bugracdnc/todoerserver/services/TodoerServiceJPA.java
+++ src/main/java/com/bugracdnc/todoerserver/services/TodoerServiceJPA.java
@@ -7,7 +7,6 @@ import com.bugracdnc.todoerserver.repo.TodoerRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
